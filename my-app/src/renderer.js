9b6d7ab9957ec9c1046cee1612b5f13aeb6336e3
Mo Chami <moeelchami@gmail.com>
2024-06-27 00:44:59 -0400

    added start game page and removed lload and new game button

--- my-app/src/renderer.js
+++ my-app/src/renderer.js
@@ -46,6 +46,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  const startGameButton = document.getElementById("start-game-button");
+  if (startGameButton) {
+    startGameButton.addEventListener("click", () => {
+      window.location.href = "game.html";
+    });
+  }
+
   const backButton = document.getElementById("back-button");
   if (backButton) {
     backButton.addEventListener("click", () => {
